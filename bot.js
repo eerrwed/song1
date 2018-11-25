2f2b5e82f5cd986a1dcfb96d10acc165debabc7e
eerrwed <45012892+eerrwed@users.noreply.github.com>
2018-11-25 20:55:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,19 @@ client.on('ready', () => {
     client.user.setGame(`Universe server.`,'https://www.twitch.tv/f9u9l1i')
     
 });
+
+
+client.on('message', message => {
+    var prefix = "1"
+    if (message.content.toLowerCase() === prefix + "help") {
+        let embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle(" :bulb: اوامر المساعدة")
+        .addField("1play",' لـتشفيل البوت ')
+        .addField("1skip",' لـخطي الاغنيه ')
+        .addField("1vol",' لـرفع صوت البوت')
+        message.channel.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
